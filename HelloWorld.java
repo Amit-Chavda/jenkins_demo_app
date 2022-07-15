6ffdddac6c78f32cb231800321b686e92d11b3dd
AMIT CHAVDA <47694676+Amit-Chavda@users.noreply.github.com>
2022-07-15 10:56:49 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld{
   public static void main(String args[]){
     System.out.println("Hello from jenkins!");
     System.out.println("Some new code");
-    
+    System.out.println("This will trigger Jenkins to build new chnages to SCM.");
   } 
 }
