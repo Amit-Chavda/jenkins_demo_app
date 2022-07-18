105e8b2753a1c8197c29090ee10ef767f20c39fe
AMIT CHAVDA <47694676+Amit-Chavda@users.noreply.github.com>
2022-07-18 17:59:12 +0530

    Updated HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld{
   public static void main(String args[]){
-    System.out.println("Hello from jenkins!");
-    System.out.println("Some new code");
-    System.out.println("This will trigger Jenkins to build new chnages to SCM.");
+    System.out.println("Project under development! Dev branch!");
   } 
 }
